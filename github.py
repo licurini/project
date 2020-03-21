af17a306aa9edd887701542fea302e092c656238
Tomasz Wichtowski <licurini@gmail.com>
2020-03-21 20:00:29 +0100

    test

--- github.py
+++ github.py
@@ -1,15 +1,18 @@
 import requests
 
-commitnumber = int(input("Write number of commits: "))
+commit_number = int(input("Write number of commits: "))
 
 def get_commits(commit_num, num_on_page, page_num):
+    commit_list = []
     commit = requests.get("https://api.github.com/repos/freeCodeCamp/freeCodeCamp/commits?per_page=100;page=(%d)" % page_num).json()
-    commit_list = commit
+    commit_list += commit
     if commit_num > num_on_page:
         commit_num = commit_num - num_on_page
         page_num += 1
-        commit_list += get_commits(commit_num, num_on_page, page_num)
-        print(commit)
+        next_page = get_commits(commit_num, num_on_page, page_num)
+        commit_list += next_page
+
+ 
     
     i = 0
     while i < commit_num:
@@ -19,5 +22,5 @@ def get_commits(commit_num, num_on_page, page_num):
         print("URL: " + commit_list[i]['url'] + "\n" + 20 * "=")
         i += 1
 
-get_commits(commitnumber, 100, 1)
+get_commits(commit_number, 100, 1)
 
