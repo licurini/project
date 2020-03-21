8da6d0d76cabde0f7c6c46d7bee51935d73cce57
Tomasz Wichtowski <licurini@gmail.com>
2020-03-21 19:42:43 +0100

    Added page management

--- github.py
+++ github.py
@@ -3,18 +3,21 @@ import requests
 commitnumber = int(input("Write number of commits: "))
 
 def get_commits(commit_num, num_on_page, page_num):
-    commit = requests.get("https://api.github.com/repos/freeCodeCamp/freeCodeCamp/commits?per_page=100;page=(%d)" % page_num) 
-    commit_list = []
+    commit = requests.get("https://api.github.com/repos/freeCodeCamp/freeCodeCamp/commits?per_page=100;page=(%d)" % page_num).json()
+    commit_list = commit
     if commit_num > num_on_page:
-        commit += get_commits(commit_num - num_on_page, num_on_page, page_num + 1)
+        commit_num = commit_num - num_on_page
+        page_num += 1
+        commit_list += get_commits(commit_num, num_on_page, page_num)
+        print(commit)
     
-    commit = commit.json()
-    commit_list += commit
-    
-
+    i = 0
+    while i < commit_num:
+        print(i + 1)
+        print("SHA: " + commit_list[i]['sha'])
+        print("Message: " + commit_list[i]['commit']['message'])
+        print("URL: " + commit_list[i]['url'] + "\n" + 20 * "=")
+        i += 1
 
 get_commits(commitnumber, 100, 1)
 
-#print("SHA: " + commit_list[0]['sha'])
-#print("Message: " + commit_list[0]['commit']['message'])
-#print("URL: " + commit_list[0]['url'])
