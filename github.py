16d00d819101dc9870bd4ad5054f9bf0dd782011
Tomasz Wichtowski <licurini@gmail.com>
2020-03-21 14:35:57 +0100

    Added requests

--- github.py
+++ github.py
@@ -1,2 +1,10 @@
-commitnumber = input("Write number of commits: ")
-print(commitnumber)
+import requests
+
+
+#commitnumber = input("Write number of commits: ")
+#print(commitnumber)
+
+commitlist = requests.get("https://api.github.com/repos/freeCodeCamp/freeCodeCamp/commits?per_page=1")
+commited = commitlist.json()
+
+print(commited[0]['sha'])
