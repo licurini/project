d93021a387bc05e938730f1f4007b93424dde56e
Tomasz Wichtowski <licurini@gmail.com>
2020-03-21 21:29:10 +0100

    Added page switching mechanism

--- github.py
+++ github.py
@@ -1,26 +1,30 @@
+#!/usr/bin/env python3
 import requests
 
 commit_number = int(input("Write number of commits: "))
 
-def get_commits(commit_num, num_on_page, page_num):
+def get_commits(commit_num, num_on_page=100, page_num=1):
     commit_list = []
-    commit = requests.get("https://api.github.com/repos/freeCodeCamp/freeCodeCamp/commits?per_page=100;page=(%d)" % page_num).json()
+    commit = requests.get("https://api.github.com/repos/freeCodeCamp/freeCodeCamp/commits?per_page={};page={}".format(num_on_page, page_num)).json()
     commit_list += commit
     if commit_num > num_on_page:
         commit_num = commit_num - num_on_page
         page_num += 1
         next_page = get_commits(commit_num, num_on_page, page_num)
         commit_list += next_page
+    else:
+        commit_list = commit_list[:commit_num]
+
+    return commit_list
 
  
-    
-    i = 0
-    while i < commit_num:
-        print(i + 1)
-        print("SHA: " + commit_list[i]['sha'])
-        print("Message: " + commit_list[i]['commit']['message'])
-        print("URL: " + commit_list[i]['url'] + "\n" + 20 * "=")
+def print_commits(commits):
+    i = 1
+    for commit in commits:
+        print(str(i) + ": SHA: " + commit['sha'])
+        print("Message: " + commit['commit']['message'])
+        print("URL: " + commit['url'] + "\n" + 20 * "=")
+        #print()
         i += 1
-
-get_commits(commit_number, 100, 1)
+print_commits(get_commits(commit_number))
 
